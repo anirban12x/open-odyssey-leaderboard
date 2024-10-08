f57e280420eb3ff6b58a6d28925b1ac3bbffb1dd
Priyobrata Mondal <priyobrata.2212@gmail.com>
2024-10-08 13:44:30 +0530

    Subho Ponchomi 🎆✨
    Updated Footer, Merged PR sorted by date, Responsiveness checks, Styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,145 +3,168 @@ import logo from "../assets/logo.png";
 import logo2 from "../assets/SamarthTMSL_logo.png";
 
 const Footer = () => {
+  const FooterLinkData = {
+    socials: [
+      {
+        icon: "fa-solid fa-globe",
+        link: "https://www.samarthtmsl.xyz",
+      },
+      {
+        icon: "fab fa-facebook-f",
+        link: "https://www.facebook.com/SamarthTMSL",
+      },
+      {
+        icon: "fab fa-instagram",
+        link: "https://www.instagram.com/samarth_tmsl_official",
+      },
+      {
+        icon: "fab fa-linkedin-in",
+        link: "https://www.linkedin.com/company/samarthtmsl",
+      },
+    ],
+    about: [
+      {
+        text: "Open Odyssey 1.0",
+        link: "#",
+      },
+      {
+        text: "Event Details",
+        link: "https://bit.ly/open-odyssey-1",
+      },
+      {
+        text: "Organizers",
+        link: "https://www.samarthtmsl.xyz",
+      },
+      {
+        text: "Contribute",
+        link: "https://linktr.ee/SamarthXHacktoberfest2024",
+      },
+    ],
+    gethelp: [
+      {
+        text: "FAQ",
+        link: "",
+      },
+      {
+        text: "Rules & Guidelines",
+        link: "",
+      },
+      {
+        text: "Technical Support",
+        link: "https://www.samarthtmsl.xyz",
+      },
+      {
+        text: "Contact Us",
+        link: "https://www.samarthtmsl.xyz",
+      },
+    ],
+    resources: [
+      {
+        text: "Leaderboard",
+        link: "#",
+      },
+      {
+        text: "Project Submissions",
+        link: "https://github.com/SamarthTech",
+      },
+      {
+        text: "Mentorship Program",
+        link: "",
+      },
+      {
+        text: "Workshop Materials",
+        link: "",
+      },
+    ],
+  };
 
-  const FooterLinkData={
-    about:[{
-      text:'Open Odyssey 1.0',
-      link:'#'
-    },
-    {
-      text:'Event Details',
-      link:'https://bit.ly/open-odyssey-1'
-    },
-    {
-      text:'Organizers',
-      link:'https://www.samarthtmsl.xyz/'
-    },
-    {
-      text:'Contribute',
-      link:'https://linktr.ee/SamarthXHacktoberfest2024'
-    },],
-    gethelp:[{
-      text:'FAQ',
-      link:''
-    },
-    {
-      text:'Rules & Guidelines',
-      link:''
-    },
-    {
-      text:'Technical Support',
-      link:'https://www.samarthtmsl.xyz/'
-    },
-    {
-      text:'Contact Us',
-      link:'https://www.samarthtmsl.xyz/'
-    },],
-    resources:[{
-      text:'Leaderboard',
-      link:'#'
-    },
-    {
-      text:'Project Submissions',
-      link:'https://github.com/SamarthTech'
-    },
-    {
-      text:'Mentorship Program',
-      link:''
-    },
-    {
-      text:'Workshop Materials',
-      link:''
-    },],
-  }
-  
   return (
     <footer
-      className="bg-[#101010] text-gray-300 py-10 px-12 mt-16 
+      className="bg-[#101010] text-gray-300 py-10 px-12 mt-20 
     border-t-2 border-t-purple-600"
     >
       <div className="container mx-auto px-5 pt-3">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* Follow Us Section */}
-          <div className="footer-col flex justify-center items-start">
+          <div className="footer-col flex justify-start lg:justify-center items-start">
             <div className="w-max">
               <div className="flex items-center flex-shrink-0">
-                <img className="h-20 w-20 mr-2" src={logo} alt="Logo" />
-                <span className="text-3xl tracking-tight text-white">Open Odyssey</span>
+                <img className="h-16 w-16 mr-2 md:h-20 md:w-20" src={logo} alt="Logo" />
+                <span className="text-3xl tracking-tight text-white">
+                  Open Odyssey
+                </span>
               </div>
               <div className="mt-5">
                 <h4 className="text-white text-xl font-semibold mb-3">
                   Follow Us
                 </h4>
                 <div className="flex space-x-4">
-                  <a href="#" className="hover:text-red-400">
-                    <i className="fab fa-facebook-f"></i>
-                  </a>
-                  <a href="#" className="hover:text-red-400">
-                    <i className="fab fa-twitter"></i>
-                  </a>
-                  <a href="#" className="hover:text-red-400">
-                    <i className="fab fa-instagram"></i>
-                  </a>
-                  <a href="#" className="hover:text-red-400">
-                    <i className="fab fa-linkedin-in"></i>
-                  </a>
+                  {FooterLinkData.socials.map((item) => (
+                    <a href={item.link} className="hover:text-red-400">
+                      <i className={item.icon}></i>
+                    </a>
+                  ))}
                 </div>
               </div>
             </div>
           </div>
 
           {/* About Us Section */}
-          <div className="footer-col flex justify-center items-start">
+          <div className="footer-col flex justify-start lg:justify-center items-start">
             <div className="w-max">
               <h4 className="text-white text-xl font-semibold mb-4">
                 About Us
               </h4>
               <ul className="space-y-2">
-                {FooterLinkData.about.map((item)=><li>
-                  <a href={item.link} className="hover:text-red-400">
-                    {item.text}
-                  </a>
-                </li>)}
-                
+                {FooterLinkData.about.map((item) => (
+                  <li>
+                    <a href={item.link} className="hover:text-red-400">
+                      {item.text}
+                    </a>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
 
           {/* Get Help Section */}
-          <div className="footer-col flex justify-center items-start">
+          <div className="footer-col flex justify-start lg:justify-center items-start">
             <div className="w-max">
               <h4 className="text-white text-xl font-semibold mb-4">
                 Get Help
               </h4>
               <ul className="space-y-2">
-              {FooterLinkData.gethelp.map((item)=><li>
-                  <a href={item.link} className="hover:text-red-400">
-                    {item.text}
-                  </a>
-                </li>)}
+                {FooterLinkData.gethelp.map((item) => (
+                  <li>
+                    <a href={item.link} className="hover:text-red-400">
+                      {item.text}
+                    </a>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
 
           {/* Resources Section */}
-          <div className="footer-col flex justify-center items-start">
+          <div className="footer-col flex justify-start lg:justify-center items-start">
             <div className="w-max">
               <h4 className="text-white text-xl font-semibold mb-4">
                 Resources
               </h4>
               <ul className="space-y-2">
-              {FooterLinkData.resources.map((item)=><li>
-                  <a href={item.link} className="hover:text-red-400">
-                    {item.text}
-                  </a>
-                </li>)}
+                {FooterLinkData.resources.map((item) => (
+                  <li>
+                    <a href={item.link} className="hover:text-red-400">
+                      {item.text}
+                    </a>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
         </div>
-        <div className="mt-12 w-full flex flex-row justify-center items-center">
-          <p className="text-xs">Copyright © 2024 - Pravidhi, Samarth TMSL</p>
+        <div className="mt-16 w-full flex flex-row justify-center items-center">
+          <p className="text-xs text-center">Copyright © 2024 - Pravidhi, Samarth TMSL</p>
         </div>
       </div>
     </footer>
--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -4,13 +4,14 @@ import codeImg from "../assets/code.jpg";
 import axios from "axios";
 
 const Workflow = () => {
+  const ViewMorePRcount = 5;
 
-  const ViewMorePRcount=5
+  const hiddenUsers = ["darkhorse404", "SamarthTech", "anirban12x"];
 
   const [repoMergedPRs, setRepoMergedPRs] = useState({
     repo1: [],
     repo2: [],
-    repo3: []
+    repo3: [],
   });
   const [overallMergedPRs, setOverallMergedPRs] = useState([]);
   const [showMore, setShowMore] = useState({
@@ -26,28 +27,45 @@ const Workflow = () => {
       if (!token) {
         console.error("GitHub token is not set");
         return [];
-      } 
+      }
 
       const prsResponse = await axios.get(
         `https://api.github.com/repos/${owner}/${repo}/pulls?state=closed&base=main&per_page=100`,
         { headers: { Authorization: `token ${token}` } }
       );
 
+      console.log(prsResponse);
+
       const mergedPRCounts = prsResponse.data.reduce((acc, pr) => {
-        const isInvalid = pr.labels.some(label => label.name.toLowerCase() === "invalid");
+        const isInvalid = pr.labels.some(
+          (label) => label.name.toLowerCase() === "invalid"
+        );
         if (pr.merged_at && !isInvalid) {
           const user = pr.user.login;
           if (!acc[user]) {
-            acc[user] = { count: 0, avatar_url: pr.user.avatar_url };
+            acc[user] = {
+              count: 0,
+              avatar_url: pr.user.avatar_url,
+              last_merge_date: pr.merged_at,
+            };
           }
           acc[user].count += 1;
+          if (new Date(pr.merged_at) > new Date(acc[user].last_merge_date))
+            acc[user].last_merge_date = pr.merged_at;
         }
         return acc;
       }, {});
 
-      return Object.entries(mergedPRCounts)
+      console.log(mergedPRCounts);
+
+      const sortedByCount = Object.entries(mergedPRCounts)
         .map(([login, data]) => ({ login, ...data }))
         .sort((a, b) => b.count - a.count);
+      return sortedByCount.sort((a, b) => {
+        if (a.count === b.count) {
+          return new Date(a.last_merge_date) - new Date(b.last_merge_date);
+        }
+      });
     } catch (error) {
       console.error(`Error fetching GitHub data for ${owner}/${repo}:`, error);
       return [];
@@ -56,14 +74,23 @@ const Workflow = () => {
 
   useEffect(() => {
     const fetchAllRepoData = async () => {
-      const repo1Data = await fetchGitHubData("SamarthTech", "web-projects-2024");
-      const repo2Data = await fetchGitHubData("SamarthTech", "python-projects-2024");
-      const repo3Data = await fetchGitHubData("SamarthTech", "noncode-projects-2024");
+      const repo1Data = await fetchGitHubData(
+        "SamarthTech",
+        "web-projects-2024"
+      );
+      const repo2Data = await fetchGitHubData(
+        "SamarthTech",
+        "python-projects-2024"
+      );
+      const repo3Data = await fetchGitHubData(
+        "SamarthTech",
+        "noncode-projects-2024"
+      );
 
       setRepoMergedPRs({
         repo1: repo1Data,
         repo2: repo2Data,
-        repo3: repo3Data
+        repo3: repo3Data,
       });
     };
 
@@ -72,21 +99,28 @@ const Workflow = () => {
 
   useEffect(() => {
     const calculateOverallLeaderboard = () => {
-      const combinedPRs = [...repoMergedPRs.repo1, ...repoMergedPRs.repo2, ...repoMergedPRs.repo3];
+      const combinedPRs = [
+        ...repoMergedPRs.repo1,
+        ...repoMergedPRs.repo2,
+        ...repoMergedPRs.repo3,
+      ];
       const overallPRs = combinedPRs.reduce((acc, pr) => {
-        if (!acc[pr.login]) {
-          acc[pr.login] = { count: 0, avatar_url: pr.avatar_url };
+        if (pr.author_association != "COLLABORATOR") {
+          if (!acc[pr.login]) {
+            acc[pr.login] = { count: 0, avatar_url: pr.avatar_url };
+          }
+          acc[pr.login].count += pr.count;
+          return acc;
         }
-        acc[pr.login].count += pr.count;
-        return acc;
       }, {});
 
       const sortedOverallPRs = Object.entries(overallPRs)
         .map(([login, data]) => ({ login, ...data }))
         .sort((a, b) => b.count - a.count);
 
-      const hiddenUsers = ["darkhorse404", "SamarthTech", "anirban12x"];
-      const filteredOverallPRs = sortedOverallPRs.filter(pr => !hiddenUsers.includes(pr.login));
+      const filteredOverallPRs = sortedOverallPRs.filter(
+        (pr) => !hiddenUsers.includes(pr.login)
+      );
 
       setOverallMergedPRs(filteredOverallPRs.slice(0, 3));
     };
@@ -95,36 +129,39 @@ const Workflow = () => {
   }, [repoMergedPRs]);
 
   const renderLeaderboard = (mergedPRs, title, isOverall, repoNo) => {
-    const hiddenUsers = ["darkhorse404", "SamarthTech", "anirban12x"];
-    const filteredMergedPRs = mergedPRs.filter(pr => !hiddenUsers.includes(pr.login));
+    const filteredMergedPRs = mergedPRs.filter(
+      (pr) => !hiddenUsers.includes(pr.login)
+    );
 
-    var showmoreRepo=null
-    if (repoNo==1)
-      showmoreRepo=showMore.repo1
-    else if (repoNo==2)
-      showmoreRepo=showMore.repo2
-    else if (repoNo==3)
-      showmoreRepo=showMore.repo3
+    var showmoreRepo = null;
+    if (repoNo == 1) showmoreRepo = showMore.repo1;
+    else if (repoNo == 2) showmoreRepo = showMore.repo2;
+    else if (repoNo == 3) showmoreRepo = showMore.repo3;
 
     const displayedPRs = filteredMergedPRs.slice(
-      0, isOverall? ViewMorePRcount: showmoreRepo
+      0,
+      isOverall ? ViewMorePRcount : showmoreRepo
     );
 
     return (
-      <div className="w-full p-2 lg:w-1/3">
-        <h3 className="mt-6 text-2xl tracking-wide text-center mb-6">{title}</h3>
+      <div className="w-full p-2 px-4 lg:w-1/3">
+        <h3 className="mt-6 text-2xl tracking-wide text-center mb-6">
+          {title}
+        </h3>
         {displayedPRs.length === 0 ? (
-          <p className="text-center text-neutral-500">No Merged Pull Requests...</p>
+          <p className="text-center text-neutral-500">
+            No Merged Pull Requests...
+          </p>
         ) : (
           displayedPRs.map((pr, index) => {
             const borderStyle = isOverall
               ? index === 0
                 ? { border: "2px solid gold" }
                 : index === 1
-                  ? { border: "2px solid silver" }
-                  : index === 2
-                    ? { border: "2px solid #cd7f32" }
-                    : {}
+                ? { border: "2px solid silver" }
+                : index === 2
+                ? { border: "2px solid #cd7f32" }
+                : {}
               : {};
 
             return (
@@ -144,8 +181,8 @@ const Workflow = () => {
                   alt={`${pr.login}'s avatar`}
                   className="w-12 h-12 mr-4 rounded-full"
                 />
-                <div className="flex-grow flex-shrink">
-                  <h5 className="text-xl">{pr.login}</h5>
+                <div className="flex-grow flex-shrink overflow-x-hidden">
+                  <h5 className="text-xl custom-word-wrap">{pr.login}</h5>
                   <p className="text-sm text-neutral-500">
                     Merged Pull Requests: {pr.count}
                   </p>
@@ -157,31 +194,45 @@ const Workflow = () => {
             );
           })
         )}
-        {filteredMergedPRs.length > ViewMorePRcount && ( 
-          <div className="w-full flex flex-row justify-center items-center mt-6 mb-2"><button
-            className="inline-block border-b-2 border-white-500 w-max text-center text-white-500 hover:text-purple-400 hover:border-purple-400 transition-all duration-100 ease-in"
-            onClick={() => {
-              //const showMoreCount=showMore
-              //showMoreCount[repoNo]= showMoreCount[repoNo]>=filteredMergedPRs.length?showMoreCount[repoNo]-ViewMorePRcount:showMoreCount[repoNo]+ViewMorePRcount
-              if (repoNo==1) {
-                setShowMore({
-                  ...showMore,
-                  repo1: showMore.repo1>=filteredMergedPRs.length?showMore.repo1-ViewMorePRcount:showMore.repo1+ViewMorePRcount,
-                });
-              } else if(repoNo==2){
-                setShowMore({
-                  ...showMore,
-                  repo2: showMore.repo2>=filteredMergedPRs.length?showMore.repo2-ViewMorePRcount:showMore.repo2+ViewMorePRcount,
-                });
-              } else if(repoNo==3){
-                setShowMore({
-                  ...showMore,
-                  repo3: showMore.repo3>=filteredMergedPRs.length?showMore.repo3-ViewMorePRcount:showMore.repo3+ViewMorePRcount,
-              })}
-            }} 
-          >
-            {showmoreRepo>=filteredMergedPRs.length ? "Show Less" : "View More"}
-          </button></div>
+        {filteredMergedPRs.length > ViewMorePRcount && (
+          <div className="w-full flex flex-row justify-center items-center mt-6 mb-2">
+            <button
+              className="inline-block border-b-2 border-white-500 w-max text-center text-white-500 px-0 hover:px-4 hover:text-purple-400 hover:border-purple-400 transition-all duration-100 ease-in"
+              onClick={() => {
+                //const showMoreCount=showMore
+                //showMoreCount[repoNo]= showMoreCount[repoNo]>=filteredMergedPRs.length?showMoreCount[repoNo]-ViewMorePRcount:showMoreCount[repoNo]+ViewMorePRcount
+                if (repoNo == 1) {
+                  setShowMore({
+                    ...showMore,
+                    repo1:
+                      showMore.repo1 >= filteredMergedPRs.length
+                        ? showMore.repo1 - ViewMorePRcount
+                        : showMore.repo1 + ViewMorePRcount,
+                  });
+                } else if (repoNo == 2) {
+                  setShowMore({
+                    ...showMore,
+                    repo2:
+                      showMore.repo2 >= filteredMergedPRs.length
+                        ? showMore.repo2 - ViewMorePRcount
+                        : showMore.repo2 + ViewMorePRcount,
+                  });
+                } else if (repoNo == 3) {
+                  setShowMore({
+                    ...showMore,
+                    repo3:
+                      showMore.repo3 >= filteredMergedPRs.length
+                        ? showMore.repo3 - ViewMorePRcount
+                        : showMore.repo3 + ViewMorePRcount,
+                  });
+                }
+              }}
+            >
+              {showmoreRepo >= filteredMergedPRs.length
+                ? "Show Less"
+                : "View More"}
+            </button>
+          </div>
         )}
       </div>
     );
@@ -202,15 +253,27 @@ const Workflow = () => {
         </div>
       </div>
 
-      <h3 className="mt-12 text-2xl tracking-wide text-center">Overall Leaderboard</h3>
+      <h3 className="mt-12 text-3xl tracking-wide text-center">
+        Overall Leaderboard
+      </h3>
       <div className="flex flex-wrap justify-center">
         {renderLeaderboard(overallMergedPRs, "The Top 3", true)}
       </div>
 
       <div className="flex flex-wrap justify-center mt-12">
-        {renderLeaderboard(repoMergedPRs.repo1, "Web Dev Projects",false , 1)}
-        {renderLeaderboard(repoMergedPRs.repo2, "Python and AIML Projects",false , 2)}
-        {renderLeaderboard(repoMergedPRs.repo3, "Low-non Code Projects", false, 3)}
+        {renderLeaderboard(repoMergedPRs.repo1, "Web Dev Projects", false, 1)}
+        {renderLeaderboard(
+          repoMergedPRs.repo2,
+          "Python and AIML Projects",
+          false,
+          2
+        )}
+        {renderLeaderboard(
+          repoMergedPRs.repo3,
+          "Low and Non Code Projects",
+          false,
+          3
+        )}
       </div>
     </div>
   );
